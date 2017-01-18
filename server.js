80581164ab7d2a9aa37e6f9df3b73ed195fc5449
alvaro <Alvarojosepl@gmail.com>
2017-01-18 09:14:30 -0600

    changed default port

--- server.js
+++ server.js
@@ -15,5 +15,4 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(3000);
-console.log('Listening on port 3000');
+app.listen(process.env.PORT || 3000);
