63b069db861379adb9d607c73daf40b53132bad2
alvaro <Alvarojosepl@gmail.com>
2017-01-18 09:06:49 -0600

    added cors

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/bundle.js', browserify(__dirname + '/index.js'));
 
 app.get('/', function(req, res){
